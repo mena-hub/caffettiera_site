0388a9ad770e11a385ba38cea4dd0458395630c1
mena-hub <mssz.nnia@gmail.com>
2020-07-27 16:44:51 -0300

    Registering social model was done

--- proyecto_cursada/social/admin.py
+++ proyecto_cursada/social/admin.py
@@ -1,1 +1,12 @@
 from django.contrib import admin
+from .models import Link
+
+class LinkAdmin(admin.ModelAdmin):
+    
+    def get_readonly_fields(self, request, obj=None):
+        if request.user.groups.filter(name="Administrador").exists():
+            return ('key', 'name')
+        else:
+            return ('created', 'updated')
+
+admin.site.register(Link, LinkAdmin)
