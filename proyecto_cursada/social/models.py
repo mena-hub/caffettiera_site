834880863762dd682b4f1c6b37c44f678300b595
mena-hub <mssz.nnia@gmail.com>
2020-07-27 16:37:22 -0300

    Defined our social model and its attributes/fields

--- proyecto_cursada/social/models.py
+++ proyecto_cursada/social/models.py
@@ -1,1 +1,16 @@
 from django.db import models
+
+class Link(models.Model):
+    key = models.SlugField(max_length=200, unique=True, verbose_name="nombre clave")
+    name = models.CharField(max_length=200, verbose_name="red social")
+    url = models.URLField(max_length=200, null=True, blank=True, verbose_name="URL")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="fecha de creación")
+    updated = models.DateTimeField(auto_now=True, verbose_name="fecha de actualización")
+
+    class Meta:
+        verbose_name = "enlace"
+        verbose_name_plural = "enlaces"
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
