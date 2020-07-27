d9d995cb5d7f947d0d72a16b2092a9076614e8a8
mena-hub <mssz.nnia@gmail.com>
2020-07-27 15:44:10 -0300

    Defined our services model and its attributes/fields

--- proyecto_cursada/services/models.py
+++ proyecto_cursada/services/models.py
@@ -1,1 +1,17 @@
 from django.db import models
+
+class Service(models.Model):
+    title = models.CharField(max_length=200, verbose_name="título")
+    subtitle = models.CharField(max_length=200, verbose_name="subtítulo")
+    content = models.TextField(verbose_name='contenido')
+    image = models.ImageField(upload_to="services", verbose_name="imagen")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="fecha de creación")
+    updated = models.DateTimeField(auto_now=True, verbose_name="fecha de actualización")
+
+    class Meta:
+        verbose_name = "servicio"
+        verbose_name_plural = "servicios"
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.title
