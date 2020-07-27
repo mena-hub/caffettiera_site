6ca0879a852a62ac5583b4e579f79ba3a91a7fd8
mena-hub <mssz.nnia@gmail.com>
2020-07-27 15:37:38 -0300

    Following views were written: services, consulta

--- proyecto_cursada/services/views.py
+++ proyecto_cursada/services/views.py
@@ -1,1 +1,16 @@
 from django.shortcuts import render
+from .models import Service
+from .consulta import return_data_from_database
+
+def services(request):
+    services = Service.objects.all()
+    return render(request, "services/services.html", 
+                    {
+                        'services':services
+                        })
+
+def consulta(request):
+    return render(request, "services/consulta.html",
+                    {
+                        'columnas':return_data_from_database
+                        })
