d7602e45cd4bfbcbca81c63e6d50618d78d18032
mena-hub <mssz.nnia@gmail.com>
2020-07-28 14:16:30 -0300

    Current view was refactored into a DRF ViewSet view

--- proyecto_djangorest/api/views.py
+++ proyecto_djangorest/api/views.py
@@ -1,1 +1,7 @@
-from django.shortcuts import render
+from .models import Product
+from .serializers import ProductSerializer
+from rest_framework import viewsets
+
+class ProductViewSet(viewsets.ModelViewSet):
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()
