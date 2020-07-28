9d6cbca480673a70a93435bff146088a32832ebe
mena-hub <mssz.nnia@gmail.com>
2020-07-28 14:11:28 -0300

    Defined our products model and its attributes/fields

--- proyecto_djangorest/api/models.py
+++ proyecto_djangorest/api/models.py
@@ -1,1 +1,13 @@
 from django.db import models
+
+class Product(models.Model):
+    name = models.CharField(max_length=150)
+    price = models.DecimalField(decimal_places=2, max_digits=5)
+    description = models.TextField(help_text="Acerca del producto.")
+    stock = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
