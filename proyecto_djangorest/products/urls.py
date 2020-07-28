8a66bb5444d9c0fb6340ea5b950496c90695ca1c
mena-hub <mssz.nnia@gmail.com>
2020-07-28 14:17:51 -0300

    Registered our view with a DRF Router and then linked to URL endpoint

--- proyecto_djangorest/products/urls.py
+++ proyecto_djangorest/products/urls.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from rest_framework import routers
+from api import views
+
+router = routers.DefaultRouter()
+router.register('products', views.ProductViewSet)
 
 urlpatterns = [
+    path('api/v1/', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
