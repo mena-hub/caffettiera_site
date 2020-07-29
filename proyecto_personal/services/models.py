411d202adc213286d88804f5ecb5ce8c3a7879c0
mena-hub <mssz.nnia@gmail.com>
2020-07-29 14:20:15 -0300

    Defined our services model and its attributes/fields

--- proyecto_personal/services/models.py
+++ proyecto_personal/services/models.py
@@ -1,1 +1,18 @@
 from django.db import models
+from ckeditor import fields
+
+class Service(models.Model):
+    title = models.CharField(max_length=200, verbose_name="título")
+    subtitle = models.CharField(max_length=200, verbose_name="subtítulo")
+    content = fields.RichTextField(verbose_name="contenido")
+    image = models.ImageField(upload_to="services", verbose_name="imagen")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="fecha de creación")
+    updated = models.DateTimeField(auto_now=True, verbose_name="fecha de actualización")
+
+    class Meta:
+        verbose_name = "servicio"
+        verbose_name_plural = "servicios"
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.title
