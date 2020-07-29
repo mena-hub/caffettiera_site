58c1fb76c1a9d55bf7e493758c7c5436f328207f
mena-hub <mssz.nnia@gmail.com>
2020-07-29 15:16:29 -0300

    Following views were written: blog, categories

--- proyecto_personal/blog/views.py
+++ proyecto_personal/blog/views.py
@@ -1,1 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Category
+
+def blog(request):
+    posts = Post.objects.all()
+    return render(request, "blog/blog.html", 
+                    {
+                        'posts':posts
+                        })
+
+def categories(request, category_id):
+    categories = get_object_or_404(Category, id=category_id)
+    return render(request, "blog/categories.html", 
+                    {
+                        'categories':categories
+                        })
