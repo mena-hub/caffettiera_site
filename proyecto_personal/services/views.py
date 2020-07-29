abad5276d38b2502d0b38443095b8e07db92e192
mena-hub <mssz.nnia@gmail.com>
2020-07-29 14:11:18 -0300

    Following view was written: services

--- proyecto_personal/services/views.py
+++ proyecto_personal/services/views.py
@@ -1,1 +1,9 @@
 from django.shortcuts import render
+from .models import Service
+
+def services(request):
+    services = Service.objects.all()[::-1]
+    return render(request, "services/services.html", 
+                    {
+                        'services':services
+                        })
